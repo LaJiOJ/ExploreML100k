14a220387a68c2e75e4f2eaee75f83a608b85dc1
LaJiOJ <1184264181@qq.com>
2020-12-21 21:36:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Information:
     def __init__(self):
         #用户、电影、评分总数
         self.user_number = 943
-        self.items_number = 1682
+        self.movie_number = 1682
         self.rating_number = 100000
         
     def load_info(self):
@@ -14,16 +14,16 @@ class Information:
         self.user_info = pd.read_table('./ml-100k/u.user', sep = '\|', names = user_names, engine = 'python')
         
         # 读入用户评分
-        rating_names = ['user id', 'item id', 'rating', 'timestamp']
+        rating_names = ['user id', 'movie id', 'rating', 'timestamp']
         self.rating_info = pd.read_table('./ml-100k/u.data', sep = '\t', names = rating_names, engine = 'python')
         
         # 读入电影信息
-        item_names = ['movie id', 'movie title', 'release date', 'video release date', 
+        movie_names = ['movie id', 'movie title', 'release date', 'video release date', 
                 'IMDb URL', 'unknown', 'Action', 'Adventure', 'Animation', 
                 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 
                 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 
                 'Thriller', 'War', 'Western']
-        self.item_info = pd.read_table('./ml-100k/u.item', sep = '\|', names = item_names, engine = 'python')
+        self.movie_info = pd.read_table('./ml-100k/u.item', sep = '\|', names = movie_names, engine = 'python')
         
         #读入电影类型编号
         genre_names = ['genre', 'index']
@@ -44,14 +44,14 @@ class Information:
 
         '''
         genre_df = pd.DataFrame()
-        genre_df = self.item_info.loc[0:self.items_number-1, 'unknown':'Western']
+        genre_df = self.movie_info.loc[0:self.movie_number-1, 'unknown':'Western']
         print(genre_df.sum())
-        return genre_df
+        return genre_df.sum()
 
 
     def question2(self):
         '''
-        计算不同职业倾向的评分高低
+        计算不同职业的评分高低
 
         Returns
         -------
@@ -61,17 +61,17 @@ class Information:
             不同职业人群给出的评分方差
 
         '''
-        user_df = pd.DataFrame()
-        user_df['user id'] = self.user_info['user id']
-        user_df['occupation'] = self.user_info['occupation']
-        rating_df = pd.DataFrame()
-        rating_df['user id'] = self.rating_info['user id']
-        rating_df['rating'] = self.rating_info['rating']
+        user_df = pd.DataFrame(self.user_info,
+                               columns=['user id', 'occupation'])
+        rating_df = pd.DataFrame(self.rating_info, 
+                                 columns=['user id', 'rating'])
         # 连接表
         uid_occu_rating_df = pd.merge(user_df, rating_df, on='user id')
         uid_occu_rating_df = uid_occu_rating_df[['occupation','rating']]
         # 分组
-        occu_rating_df = uid_occu_rating_df.groupby("occupation")
+        occu_rating_df = uid_occu_rating_df.groupby('occupation')
+        # # 初步展示结果
+        # occu_rating_df.agg(['mean','var']).plot(kind='bar')
         # 平均值
         occu_rating_mean_df = occu_rating_df.agg('mean').sort_values(by='rating',ascending = False   )
         # 方差
@@ -80,6 +80,36 @@ class Information:
     
     def question3(self):
         '''
+        计算不同性别不同年龄段的评分
+
+        Returns
+        -------
+        age_gender_rating_mean_df : DataFrame
+            不同性别不同年龄段的评分平均数
+        age_gender_rating_var_df : DataFrame
+            不同性别不同年龄段的评分方差
+
+        '''
+        user_df = pd.DataFrame(self.user_info,
+                               columns=['user id', 'age', 'gender'])
+        rating_df = pd.DataFrame(self.rating_info, 
+                                 columns=['user id', 'rating'])
+        # 连接表
+        uid_age_rating_df = pd.merge(user_df, rating_df, on='user id')
+        uid_age_rating_df = uid_age_rating_df[['age','gender','rating']]
+        # 年龄分组
+        age_group = pd.cut(uid_age_rating_df['age'],bins=[0,18,25,35,60,100])# cut左开右闭，且数据集中没有>100岁的人
+        age_gender_rating_df = uid_age_rating_df.groupby([age_group,'gender'])
+        # # 初步展示结果
+        # age_gender_rating_df.agg(['mean','var']).plot(kind='bar')
+        # 平均值
+        age_gender_rating_mean_df = age_gender_rating_df.agg('mean').sort_values(by='rating', ascending=False)['rating']
+        # 方差
+        age_gender_rating_var_df = age_gender_rating_df.agg('var').sort_values(by='rating')['rating']
+        return age_gender_rating_mean_df, age_gender_rating_var_df
+    
+    def question4(self):
+        '''
         不同职业喜爱的电影类型
 
         Returns
@@ -87,27 +117,37 @@ class Information:
         None.
 
         '''
-        user_df = pd.DataFrame()
-        user_df['user id'] = self.user_info['user id']
-        user_df['item id'] = self.user_info['item id']
-        user_df['occupation'] = self.user_info['occupation']
-        rating_df = pd.DataFrame()
-        rating_df['user id'] = self.rating_info['user id']
-        rating_df['rating'] = self.rating_info['rating']
-        item_df = pd.DataFrame()
-        item_df['item id'] = self.item_info['movie id']
-        # TODO: 把不同类型电影加进来
+        user_df = pd.DataFrame(self.user_info, columns=['user id', 'occupation'])
         
-    
-    
-
+        rating_df = pd.DataFrame(self.rating_info, columns=['user id', 'movie id', 'rating'])
+        
+        columns_need = ['movie id']
+        columns_need.extend(self.movie_info.columns.tolist()[5:])
+        movie_df = pd.DataFrame(self.movie_info, columns = columns_need)
+        
+        user_rating_df = pd.merge(user_df, rating_df, on='user id')
+        
+        user_rating_movie_df = pd.merge(user_rating_df,
+                                       movie_df,
+                                       how='left',
+                                       on='movie id')
+        
+        # 分组
+        user_rating_movie_df = user_rating_movie_df.groupby('occupation')
+        
+        return user_rating_movie_df
+        
+        
 if __name__ == '__main__':
     info = Information()
     info.load_info()
+    # genre_df = info.question1()
     
-    info.question2()
+    # occu_rating_mean_df, occu_rating_var_df = info.question2()
     
+    # age_rating_mean_df, age_rating_var_df = info.question3()
     
+    user_rating_movie_df = info.question4()    
 
 
 
